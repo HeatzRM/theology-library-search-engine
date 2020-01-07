7fbc7b876fa8ac86680d0585d73ae50c8149c0df
Rotherford Marmibe <rmarmibe@gmail.com>
2020-01-07 20:05:47 +0800

    update requirements

--- app/textmining.py
+++ app/textmining.py
@@ -59,11 +59,11 @@ class TextMiner(object):
 
 """		
 print(TextMiner().get_cluster_terms("devices", ["This little kitty came to play when I was eating at a restaurant.",
-             "Merley has the best squooshy kitten belly.",
-             "Google Translate app is incredible.",
-             "If you open 100 tab in google you get a smiley face.",
-             "Best cat photo I've ever taken.",
-             "Climbing ninja cat.",
-             "Impressed with google map feedback.",
-             "Key promoter extension for Google Chrome."])) 
+"Merley has the best squooshy kitten belly.",
+"Google Translate app is incredible.",
+"If you open 100 tab in google you get a smiley face.",
+"Best cat photo I've ever taken.",
+"Climbing ninja cat.",
+"Impressed with google map feedback.",
+"Key promoter extension for Google Chrome."])) 
 """
