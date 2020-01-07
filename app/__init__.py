a4d8dcebcd160dfe190764de4618e03c348cc097
Rotherford Marmibe <rmarmibe@gmail.com>
2020-01-07 20:11:14 +0800

    add heroku config

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -17,22 +17,27 @@ login = LoginManager(app)
 login.login_view = "login"
 
 if not app.debug:
-    if not os.path.exists("logs"):
-        os.mkdir("logs")
-    file_handler = RotatingFileHandler(
-        "logs/library.log", maxBytes=10240, backupCount=10
-    )
-    file_handler.setFormatter(
-        logging.Formatter(
-            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists("logs"):
+            os.mkdir("logs")
+        file_handler = RotatingFileHandler(
+            "logs/application.log", maxBytes=10240, backupCount=10
         )
-    )
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+        file_handler.setFormatter(
+            logging.Formatter(
+                "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+            )
+        )
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
     app.logger.setLevel(logging.INFO)
-    app.logger.info("Library startup")
+    app.logger.info("Application startup")
 
-from app import routes, models, errors
+from app import routes, models
 
 port = int(os.environ.get("PORT", 80))
 waitress.serve(app, port=port)
--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
-    SECRET_KEY = os.environ.get("heatz") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+    SECRET_KEY = 'ab9af4c976dbb8c664fb205079b0e94d013572ebfd755c13af2b7b95b988b843'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
